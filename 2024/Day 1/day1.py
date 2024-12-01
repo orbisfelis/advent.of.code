e66b501760f7b995e87feb99b7b193f916ffd991
Milen K <mkindekov@gmail.com>
2024-12-01 18:31:59 +0000

    Day 1 Advent of Code 2024 - 2 parts

---
+++ 2024/Day 1/day1.py
@@ -0,0 +1,35 @@
+from heapq import heappush, heappop
+from collections import Counter
+
+def part1():
+    heap1, heap2 = [], []
+    sum = 0
+    with open("2024/Day 1/input.txt", 'r') as file:
+        lines = [line.rstrip() for line in file]
+        for line in lines:
+            inputs = line.split("   ")
+            heappush(heap1, int(inputs[0]))
+            heappush(heap2, int(inputs[1]))
+    while heap1 and heap2:
+        sum += abs(heappop(heap1) - heappop(heap2))
+    print(sum)
+
+part1()
+
+def part2():
+    inputs1, inputs2 = [], []
+    sum = 0
+    with open("2024/Day 1/input.txt", 'r') as file:
+        lines = [line.rstrip() for line in file]
+        for line in lines:
+            inputs = line.split("   ")
+            inputs1.append(int(inputs[0]))
+            inputs2.append(int(inputs[1]))
+        
+        counts = Counter(inputs2)
+        for coordinate in inputs1:
+            sum += coordinate * counts.get(coordinate, 0)
+        
+    print(sum)
+
+part2()
