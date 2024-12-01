cb68bba12328c1eac5e830fca931de99331ce235
Milen K <mkindekov@gmail.com>
2024-12-01 18:32:28 +0000

    A few tweaks to variable names.

--- 2024/Day 1/day1.py
+++ 2024/Day 1/day1.py
@@ -7,9 +7,9 @@ def part1():
     with open("2024/Day 1/input.txt", 'r') as file:
         lines = [line.rstrip() for line in file]
         for line in lines:
-            inputs = line.split("   ")
-            heappush(heap1, int(inputs[0]))
-            heappush(heap2, int(inputs[1]))
+            coordinates = line.split("   ")
+            heappush(heap1, int(coordinates[0]))
+            heappush(heap2, int(coordinates[1]))
     while heap1 and heap2:
         sum += abs(heappop(heap1) - heappop(heap2))
     print(sum)
@@ -22,9 +22,9 @@ def part2():
     with open("2024/Day 1/input.txt", 'r') as file:
         lines = [line.rstrip() for line in file]
         for line in lines:
-            inputs = line.split("   ")
-            inputs1.append(int(inputs[0]))
-            inputs2.append(int(inputs[1]))
+            coordinates = line.split("   ")
+            inputs1.append(int(coordinates[0]))
+            inputs2.append(int(coordinates[1]))
         
         counts = Counter(inputs2)
         for coordinate in inputs1:
