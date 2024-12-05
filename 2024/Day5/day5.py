74a6145f0ac7172804b55ed3c32db134871fa3a7
Milen K <mkindekov@gmail.com>
2024-12-05 10:17:31 +0000

    day5 - cleanup

--- 2024/Day5/day5.py
+++ 2024/Day5/day5.py
@@ -5,32 +5,32 @@ class Day5:
     def read_lines(self, filepath: str) -> list:
         return [line.rstrip() for line in open(filepath, 'r')]
 
-    def check_valid(self, rules_post: list, orders: list):
+    def check_valid(self, rules: dict, orders: list):
         for idx, page in enumerate(orders):
             for after_page in orders[idx+1:]:
-                if page in rules_post[after_page]:
+                if page in rules[after_page]:
                     return False
         return True
 
-    def sort_via_rules(self, rules_post: list, orders: list):
+    def sort_via_rules(self, rules: dict, orders: list):
         def compare(a: int, b: int) -> int:
-            return 1 if a in rules_post[b] else -1
+            return 1 if a in rules[b] else -1
         ordered_list = sorted(orders, key=cmp_to_key(compare))
         return ordered_list
     
-    def part1(self, rules_post: list, print_order: list):
+    def part1(self, rules: dict, print_order: list):
         sum = 0
         for orders in print_order:
-            if self.check_valid(rules_post, orders):
+            if self.check_valid(rules, orders):
                 middle_index = (len(orders) - 1)//2
                 sum += orders[middle_index]
         print(sum)
 
-    def part2(self, rules_post: list, print_order: list):
+    def part2(self, rules: dict, print_order: list):
         sum = 0
         for orders in print_order:
-            if not self.check_valid(rules_post, orders):
-                sorted = self.sort_via_rules(rules_post, orders)
+            if not self.check_valid(rules, orders):
+                sorted = self.sort_via_rules(rules, orders)
                 middle_index = (len(sorted) - 1)//2
                 sum += sorted[middle_index]
         print(sum)
@@ -38,21 +38,21 @@ class Day5:
 def main():
     solution = Day5()
     file = solution.read_lines("2024/Day5/test_input.txt")
-    rules_post = {}
+    rules = {}
     print_order = []
     for line in file:
         if '|' in line:
             src, dst = map(int, line.split('|'))
-            if src not in rules_post:
-                rules_post[src] = []
-            if dst not in rules_post:
-                rules_post[dst] = []
-            rules_post[src].append(dst)
+            if src not in rules:
+                rules[src] = []
+            if dst not in rules:
+                rules[dst] = []
+            rules[src].append(dst)
         if line == '':
             continue
         if "," in line:
             print_order.append(list(map(int, line.split(','))))
-    solution.part1(rules_post, print_order)
-    solution.part2(rules_post, print_order)
+    solution.part1(rules, print_order)
+    solution.part2(rules, print_order)
 
 main()
